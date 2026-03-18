7b3ba7ef3ffa0b902c8714560165cf0a1fb528b1
Umer Mehmood <103528755+Umerrrdo@users.noreply.github.com>
2026-03-18 22:24:27 +0500

    feat: GTM tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,15 @@ export default function RootLayout({
         <meta name="geo.placename" content="Washington" />
       </Head>
 
+      {/* Google Tag Manager */}
+      <Script id="gtm-js" strategy="beforeInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TSWJXC32');`}
+      </Script>
+
       {/* Google Analytics */}
       <Script
         async
@@ -93,6 +102,15 @@ export default function RootLayout({
         defer
       />
       <body className={`${poppins.className} ${dmSans.className}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TSWJXC32"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Suspense fallback={<div>Loading...</div>}>
           <Provider store={store}>
             <AppWrappers>{children}</AppWrappers>
